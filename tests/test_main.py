742a7b448f84b21fbe76e48f05f128b770f3e14c
Jan Zarsky <jzarsky@redhat.com>
2019-03-11 09:45:12 +0100

    Create basic tests
    Add testing JSON files generated by podman and docker. Add expected
    output cil policies. Add tests that run udica on testing JSON files and
    compare the result with cil policies.
    
    The tests should be run inside the 'tests' directory using unittest:
    
        # python3 -m unittest
    
    The tests are intended to be run on Fedora machine as root. Tested on
    Fedora 29.

---
+++ tests/test_main.py
@@ -0,0 +1,90 @@
+# Copyright (C) 2019 Jan Zarsky, <jzarsky@redhat.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+import sys
+import unittest
+from unittest.mock import patch
+
+sys.path.insert(0, os.path.abspath('..'))
+import udica.__main__
+
+class TestMain(unittest.TestCase):
+    """Test basic functionality of udica"""
+
+    def test_basic_podman(self):
+        """podman run -v /home:/home:ro -v /var/spool:/var/spool:rw -p 21:21 fedora"""
+        args = ['udica', '-j', 'test_basic.podman.json', 'my_container']
+        self.helper(args, 'test_basic.podman.cil',
+                    '{base_container.cil,net_container.cil,home_container.cil}')
+
+    def test_basic_docker(self):
+        """docker run -v /home:/home:ro -v /var/spool:/var/spool:rw -p 21:21 fedora"""
+        args = ['udica', '-j', 'test_basic.docker.json', 'my_container']
+        self.helper(args, 'test_basic.docker.cil',
+                    '{base_container.cil,net_container.cil,home_container.cil}')
+
+    def test_default_podman(self):
+        """podman run fedora"""
+        args = ['udica', '-j', 'test_default.podman.json', 'my_container']
+        self.helper(args, 'test_default.podman.cil', 'base_container.cil')
+
+    def test_default_docker(self):
+        """docker run fedora"""
+        args = ['udica', '-j', 'test_default.docker.json', 'my_container']
+        self.helper(args, 'test_default.docker.cil', 'base_container.cil')
+
+    def helper(self, args, policy_file=None, templates=None):
+        """Run udica with args, check output and used templates.
+
+        Arguments:
+        args -- list of program arguments (the first one is an executable name)
+        policy_file -- check that output of udica matches this file
+        templates -- check that these templates are part of udica output, e.g. 'base_container.cil'
+            or '{base_container.cil,net_container.cil}'
+        """
+        # FIXME: the policy module is using global variable which must be reset to []
+        udica.policy.templates_to_load = []
+        # FIXME: the load_policy function is not properly restoring current working directory
+        self.cwd = os.getcwd()
+
+        with patch('sys.argv', args):
+            with patch('sys.stderr.write') as mock_err, patch('sys.stdout.write') as mock_out:
+                mock_out.output = ""
+                def store_output(output):
+                    mock_out.output += output
+                mock_out.side_effect = store_output
+                udica.__main__.main()
+                mock_err.assert_not_called()
+
+        self.assertRegex(mock_out.output, 'Policy my_container created')
+        self.assertRegex(mock_out.output, '--security-opt label=type:my_container.process')
+        self.assertRegex(mock_out.output, 'semodule -i my_container')
+
+        if templates:
+            self.assertRegex(mock_out.output, udica.policy.TEMPLATES_STORE + '/' + templates)
+
+        os.chdir(self.cwd)
+
+        self.assertTrue(os.path.isfile('my_container.cil'))
+
+        if policy_file:
+            with open('my_container.cil') as cont:
+                policy = cont.read().strip()
+            with open(policy_file) as cont:
+                exp_policy = cont.read().strip()
+            self.assertMultiLineEqual(policy, exp_policy)
+
+        os.unlink('my_container.cil')
