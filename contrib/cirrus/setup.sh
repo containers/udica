2f511c7c4b65e81df34c5710b873cf687fd6f2db
Chris Evich <cevich@redhat.com>
2019-07-19 11:55:11 -0400

    Add initial support for Cirrus-CI testing
    Signed-off-by: Chris Evich <cevich@redhat.com>

---
+++ contrib/cirrus/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -eo pipefail
+
+source "$(dirname $0)/lib.sh"
+
+case "${OS_RELEASE_ID}" in
+    fedora)
+        echo "Installing necessary additional packages"
+        ooe.sh dnf install -y \
+            python3 \
+            setools-console \
+            systemd-devel
+        ;;
+    *) bad_os_id_ver ;;
+esac
+
+echo "Configuring git for access to podman pull-requests"
+NEWREF='+refs/pull/*/head:refs/remotes/upstream/pr/*'
+git config --global --replace-all remote.origin.fetch "$NEWREF"
+# helpful when debugging w/ hack/get_ci_vm.sh
+git config --global --replace-all remote.upstream.fetch "$NEWREF"
