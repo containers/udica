98f6d4ee7c8a700c08984d24838dfb05e1794f9c
Lukas Vrabec <lvrabec@redhat.com>
2019-10-25 22:08:41 +0200

    Bump release

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="udica",
-    version="0.2.0",
+    version="0.2.1",
     author="Lukas Vrabec",
     author_email="lvrabec@redhat.com",
     description="A tool for generating SELinux security policies for containers",
