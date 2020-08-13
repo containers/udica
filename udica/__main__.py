95ccc1cdbffbe3bd03459e57b4a3a629a15ab02b
Ondrej Mosnacek <omosnace@redhat.com>
2020-08-13 14:36:00 +0200

    Add --version command-line argument
    Create a sub-module to store the version string and use it both in
    setup.py (to set version in package metadata) and in __main__.py (to
    implement the --version command-line argument).
    
    Fixes: https://github.com/containers/udica/issues/66
    Signed-off-by: Ondrej Mosnacek <omosnace@redhat.com>

--- udica/__main__.py
+++ udica/__main__.py
@@ -19,6 +19,7 @@ import argparse
 # import udica
 from udica.parse import parse_inspect, parse_avc_file
 from udica.parse import ENGINE_ALL, ENGINE_PODMAN, ENGINE_DOCKER
+from udica.version import version
 from udica import parse
 from udica.policy import create_policy, load_policy, generate_playbook
 
@@ -27,6 +28,7 @@ def get_args():
     parser = argparse.ArgumentParser(
         description="Script generates SELinux policy for running container."
     )
+    parser.add_argument("-V", "--version", action="version", version=version)
     parser.add_argument(
         type=str, help="Name for SELinux policy module", dest="ContainerName"
     )
