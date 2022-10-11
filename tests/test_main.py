51ccb324575dc63751b5727ac52a46ade227f096
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-11 19:41:43 +0000

    Adding tarfile member sanitization to extractall()

--- tests/test_main.py
+++ tests/test_main.py
@@ -398,7 +398,26 @@ class TestBase(unittest.TestCase):
             output, "Ansible playbook and archive with udica policies generated!"
         )
         with tarfile.open("my_container-policy.tar.gz") as archive:
-            archive.extractall()
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive)
 
         self.assertTrue(os.path.isfile("deploy-module.yml"))
 
