containers/udica

commit 7c7b9ad505ab6b7cd809d30f1699d4bb7323ce0a

Avoid duplicate rules for accessing  mounts and devices
list_contexts may return duplicate contexts. This combined with multiple
mounts/devices that may share contexts leads to many duplicate allow
rules.

Example:
  tests/test_basic.podman.cil:8-11
  tests/test_basic.podman.cil:392-395
  duplicate RW mount permissions for var_spool_t

  tests/test_basic.podman.cil:28-31
  tests/test_basic.podman.cil:264-267
  tests/test_basic.podman.cil:304-307
  duplicate RW mount permissions for abrt_retrace_spool_t

This patch significantly reduces most test cil policies
e.g. test_basic.podman.cil 396 -> 253 lines
     test_basic.docker.cil 394 -> 254 lines

Signed-off-by: Vit Mojzis <vmojzis@redhat.com>

udica/policy.py
@@ -207,7 +207,10 @@ def create_policy(
 
 
 def write_policy_for_crio_mounts(mounts, policy):
-    for item in sorted(mounts, key=lambda x: str(x["hostPath"])):
+    contexts = []
+    contexts_readonly = []
+
+    for item in mounts:
         if item["hostPath"].startswith("/var/lib/kubelet"):
             # These should already have the right context
             continue
@@ -245,90 +248,100 @@ def write_policy_for_crio_mounts(mounts, policy):
 
         # TODO(jaosorior): Add prefix-dir to path. This way we could call this
         # from a container in kubernetes
-        contexts = list_contexts(item["hostPath"])
-        for context in contexts:
-            if item["readonly"] is False:
-                policy.write(
-                    "    (allow process "
-                    + context
-                    + " ( dir ( "
-                    + perms.perm["dir_rw"]
-                    + " ))) \n"
-                )
-                policy.write(
-                    "    (allow process "
-                    + context
-                    + " ( file ( "
-                    + perms.perm["file_rw"]
-                    + " ))) \n"
-                )
-                policy.write(
-                    "    (allow process "
-                    + context
-                    + " ( fifo_file ( "
-                    + perms.perm["fifo_rw"]
-                    + " ))) \n"
-                )
-                policy.write(
-                    "    (allow process "
-                    + context
-                    + " ( sock_file ( "
-                    + perms.perm["socket_rw"]
-                    + " ))) \n"
-                )
-            else:
-                policy.write(
-                    "    (allow process "
-                    + context
-                    + " ( dir ( "
-                    + perms.perm["dir_ro"]
-                    + " ))) \n"
-                )
-                policy.write(
-                    "    (allow process "
-                    + context
-                    + " ( file ( "
-                    + perms.perm["file_ro"]
-                    + " ))) \n"
-                )
-                policy.write(
-                    "    (allow process "
-                    + context
-                    + " ( fifo_file ( "
-                    + perms.perm["fifo_ro"]
-                    + " ))) \n"
-                )
-                policy.write(
-                    "    (allow process "
-                    + context
-                    + " ( sock_file ( "
-                    + perms.perm["socket_ro"]
-                    + " ))) \n"
-                )
+        if item["readonly"] is False:
+            contexts.extend(list_contexts(item["hostPath"]))
+        else:
+            contexts_readonly.extend(list_contexts(item["hostPath"]))
+
+    for context in sorted(set(contexts)):
+        policy.write(
+            "    (allow process "
+            + context
+            + " ( dir ( "
+            + perms.perm["dir_rw"]
+            + " ))) \n"
+        )
+        policy.write(
+            "    (allow process "
+            + context
+            + " ( file ( "
+            + perms.perm["file_rw"]
+            + " ))) \n"
+        )
+        policy.write(
+            "    (allow process "
+            + context
+            + " ( fifo_file ( "
+            + perms.perm["fifo_rw"]
+            + " ))) \n"
+        )
+        policy.write(
+            "    (allow process "
+            + context
+            + " ( sock_file ( "
+            + perms.perm["socket_rw"]
+            + " ))) \n"
+        )
+
+    for contexts in sorted(set(contexts_readonly)):
+        policy.write(
+            "    (allow process "
+            + context
+            + " ( dir ( "
+            + perms.perm["dir_ro"]
+            + " ))) \n"
+        )
+        policy.write(
+            "    (allow process "
+            + context
+            + " ( file ( "
+            + perms.perm["file_ro"]
+            + " ))) \n"
+        )
+        policy.write(
+            "    (allow process "
+            + context
+            + " ( fifo_file ( "
+            + perms.perm["fifo_ro"]
+            + " ))) \n"
+        )
+        policy.write(
+            "    (allow process "
+            + context
+            + " ( sock_file ( "
+            + perms.perm["socket_ro"]
+            + " ))) \n"
+        )
 
 
 def write_policy_for_podman_devices(devices, policy):
-    for item in sorted(devices, key=lambda x: str(x["PathOnHost"])):
-        contexts = list_contexts(item["PathOnHost"])
-        for context in contexts:
-            policy.write(
-                "    (allow process "
-                + context
-                + " ( blk_file ( "
-                + perms.perm["device_rw"]
-                + " ))) \n"
-            )
-            policy.write(
-                "    (allow process "
-                + context
-                + " ( chr_file ( "
-                + perms.perm["device_rw"]
-                + " ))) \n"
-            )
+    contexts = []
+
+    for item in devices:
+        contexts.extend(list_contexts(item["PathOnHost"]))
+
+    for context in sorted(set(contexts)):
+        policy.write(
+            "    (allow process "
+            + context
+            + " ( blk_file ( "
+            + perms.perm["device_rw"]
+            + " ))) \n"
+        )
+        policy.write(
+            "    (allow process "
+            + context
+            + " ( chr_file ( "
+            + perms.perm["device_rw"]
+            + " ))) \n"
+        )
 
 
 def write_policy_for_podman_mounts(mounts, policy):
-    for item in sorted(mounts, key=lambda x: str(x["Source"])):
+    contexts = []
+    contexts_rw = []
+
+    for item in mounts:
         if not item["Source"].find("/"):
             if item["Source"] == LOG_CONTAINER and item["RW"] is False:
                 policy.write("    (blockinherit log_container)\n")
@@ -370,66 +383,70 @@ def write_policy_for_podman_mounts(mounts, policy):
                 add_template("config_container")
                 continue
 
-            contexts = list_contexts(item["Source"])
-            for context in contexts:
-                if item["RW"] is True:
-                    policy.write(
-                        "    (allow process "
-                        + context
-                        + " ( dir ( "
-                        + perms.perm["dir_rw"]
-                        + " ))) \n"
-                    )
-                    policy.write(
-                        "    (allow process "
-                        + context
-                        + " ( file ( "
-                        + perms.perm["file_rw"]
-                        + " ))) \n"
-                    )
-                    policy.write(
-                        "    (allow process "
-                        + context
-                        + " ( fifo_file ( "
-                        + perms.perm["fifo_rw"]
-                        + " ))) \n"
-                    )
-                    policy.write(
-                        "    (allow process "
-                        + context
-                        + " ( sock_file ( "
-                        + perms.perm["socket_rw"]
-                        + " ))) \n"
-                    )
-                if item["RW"] is False:
-                    policy.write(
-                        "    (allow process "
-                        + context
-                        + " ( dir ( "
-                        + perms.perm["dir_ro"]
-                        + " ))) \n"
-                    )
-                    policy.write(
-                        "    (allow process "
-                        + context
-                        + " ( file ( "
-                        + perms.perm["file_ro"]
-                        + " ))) \n"
-                    )
-                    policy.write(
-                        "    (allow process "
-                        + context
-                        + " ( fifo_file ( "
-                        + perms.perm["fifo_ro"]
-                        + " ))) \n"
-                    )
-                    policy.write(
-                        "    (allow process "
-                        + context
-                        + " ( sock_file ( "
-                        + perms.perm["socket_ro"]
-                        + " ))) \n"
-                    )
+            if item["RW"] is True:
+                contexts_rw.extend(list_contexts(item["Source"]))
+            else:
+                contexts.extend(list_contexts(item["Source"]))
+
+    for context in sorted(set(contexts_rw)):
+        policy.write(
+            "    (allow process "
+            + context
+            + " ( dir ( "
+            + perms.perm["dir_rw"]
+            + " ))) \n"
+        )
+        policy.write(
+            "    (allow process "
+            + context
+            + " ( file ( "
+            + perms.perm["file_rw"]
+            + " ))) \n"
+        )
+        policy.write(
+            "    (allow process "
+            + context
+            + " ( fifo_file ( "
+            + perms.perm["fifo_rw"]
+            + " ))) \n"
+        )
+        policy.write(
+            "    (allow process "
+            + context
+            + " ( sock_file ( "
+            + perms.perm["socket_rw"]
+            + " ))) \n"
+        )
+
+    for context in sorted(set(contexts)):
+        policy.write(
+            "    (allow process "
+            + context
+            + " ( dir ( "
+            + perms.perm["dir_ro"]
+            + " ))) \n"
+        )
+        policy.write(
+            "    (allow process "
+            + context
+            + " ( file ( "
+            + perms.perm["file_ro"]
+            + " ))) \n"
+        )
+        policy.write(
+            "    (allow process "
+            + context
+            + " ( fifo_file ( "
+            + perms.perm["fifo_ro"]
+            + " ))) \n"
+        )
+        policy.write(
+            "    (allow process "
+            + context
+            + " ( sock_file ( "
+            + perms.perm["socket_ro"]
+            + " ))) \n"
+        )
 
 
 def write_policy_for_containerd_mounts(mounts, policy):

commit d20605ff28c0492e8cd30a156790cb2f47397841

Fix generating policy for Crio mounts
Fix issue introduced by
Commit 7c7b9ad505ab6b7cd809d30f1699d4bb7323ce0a
"Avoid duplicate rules for accessing  mounts and devices"
where policy rules for "read-only mounts" are not generated properly.

Adjust Crio basic test to incorporate a read only mount that is not
covered by a special case ("/home" is handled by "home_container" and
anything under "/var/lib/kubelet" is ignored).

Thanks https://github.com/arcardon (jamjcardona@sbcglobal.net) for
spotting this in the code.

Signed-off-by: Vit Mojzis <vmojzis@redhat.com>

udica/policy.py
@@ -284,7 +284,7 @@ def write_policy_for_crio_mounts(mounts, policy):
             + " ))) \n"
         )
 
-    for contexts in sorted(set(contexts_readonly)):
+    for context in sorted(set(contexts_readonly)):
         policy.write(
             "    (allow process "
             + context

tests/selinux.py
@@ -25,6 +25,8 @@ def selabel_lookup(selabel, directory, rc):
         return (0, None)
     elif directory == "/dev/fb0":
         return (0, "system_u:object_r:framebuf_device_t:s0")
+    elif directory == "/etc/hosts":
+        return (0, "system_u:object_r:net_conf_t:s0")
     else:
         return (0, "system_u:object_r:var_spool_t:s0")
 
@@ -32,5 +34,7 @@ def selabel_lookup(selabel, directory, rc):
 def getfilecon(directory):
     if directory == "/tmp/test":
         return (0, "system_u:object_r:user_tmp_t:s0")
+    elif directory == "/etc/hosts":
+        return (0, "system_u:object_r:net_conf_t:s0")
     else:
         return (0, "system_u:object_r:var_spool_t:s0")

tests/test_basic.cri.cil
@@ -250,4 +250,8 @@
     (allow process zoneminder_spool_t ( file ( append create getattr ioctl lock map open read rename setattr unlink write ))) 
     (allow process zoneminder_spool_t ( fifo_file ( getattr read write append ioctl lock open ))) 
     (allow process zoneminder_spool_t ( sock_file ( append getattr open read write ))) 
+    (allow process net_conf_t ( dir ( getattr ioctl lock open read search ))) 
+    (allow process net_conf_t ( file ( getattr ioctl lock open read ))) 
+    (allow process net_conf_t ( fifo_file ( getattr open read lock ioctl ))) 
+    (allow process net_conf_t ( sock_file ( getattr open read ))) 
 )

tests/test_main.py
@@ -119,7 +119,7 @@ class TestBase(unittest.TestCase):
         self.assert_policy(test_file("test_basic.docker.cil"))
 
     def test_basic_cri(self):
-        """Start CRI-O mounting /var/spool with read/write perms and /home with readonly perms"""
+        """Start CRI-O mounting /var/spool with read/write perms and /home and /etc/hosts with readonly perms"""
         output = self.run_udica(
             [
                 "udica",
@@ -135,7 +135,7 @@ class TestBase(unittest.TestCase):
         self.assert_policy(test_file("test_basic.cri.cil"))
 
     def test_basic_specified_engine_cri(self):
-        """Start CRI-O mounting /var/spool with read/write perms and /home with readonly perms"""
+        """Start CRI-O mounting /var/spool with read/write perms and /home and /etc/hosts with readonly perms"""
         output = self.run_udica(
             [
                 "udica",